22eb2d6ebc9136007c3655ab8c1bbfc0652ffedd
Em Dash <mbyx.dev@gmail.com>
2025-01-13 23:50:30 +0500

    feat: Added richer support for different terrain.

--- common/map/map.gd
+++ common/map/map.gd
@@ -20,21 +20,22 @@ class_name BattleMap
 @export var UnitLayer: TileMapLayer
 @export var UILayer: TileMapLayer
 
-# NOTE: Terrains that can only be specified via the Foreground must always
+# NOTE: Terrains that can only be specified via the Foreground or above must always
 # have a larger integer value than the largest terrain type specified via
 # the Background.
-# TODO: Add buildings.
+# TODO: Fix tileset assets to actually use cut down tiles instead of master asset list.
 enum TerrainType {
 	PLAINS    = 0,
 	WATER     = 1,
 	ROAD      = 2,
 	MOUNTAIN  = 3,
-	FOREST    = 4
+	FOREST    = 4,
+	BUILDING  = 5,
 }
 
 const VALUE_TO_TERRAIN: Dictionary = { 0 : TerrainType.PLAINS, 1 : TerrainType.WATER,
 									   2 : TerrainType.ROAD, 3 : TerrainType.MOUNTAIN,
-									   4 : TerrainType.FOREST }
+									   4 : TerrainType.FOREST, 5 : TerrainType.BUILDING }
 
 @export_group("Attributes")
 ## A list of resources whose indices correspond to the terrain type.
@@ -48,6 +49,7 @@ const VALUE_TO_TERRAIN: Dictionary = { 0 : TerrainType.PLAINS, 1 : TerrainType.W
 func get_terrain_type_at(pos: Vector2) -> TerrainType:
 	var bg_type: int = -1
 	var fg_type: int = -1
+	var building_type: int = -1
 
 	var bg_data: TileData = TerrainBGLayer \
 		.get_cell_tile_data(TerrainBGLayer.local_to_map(pos))
@@ -59,8 +61,13 @@ func get_terrain_type_at(pos: Vector2) -> TerrainType:
 	if fg_data:
 		bg_type = fg_data.get_custom_data("Terrain Type")
 
+	var building_data: TileData = BuildingLayer \
+		.get_cell_tile_data(BuildingLayer.local_to_map(pos))
+	if building_data:
+		building_type = building_data.get_custom_data("Terrain Type")
+
 	# Assuming the foreground terrains start after the background terrains.
-	return VALUE_TO_TERRAIN[max(bg_type, fg_type)]
+	return VALUE_TO_TERRAIN[max(bg_type, fg_type, building_type)]
 
 ## Get the terrain data of the tile at that position.
 ##
